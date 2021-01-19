20541350bdb7bc8dfffdb9d0c678d5607109f26d
park-moen <pmj6516@naver.com>
2021-01-19 20:46:51 +0900

    fix saga

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import NotFoundPage from './pages/NotFoundPage';
-import HomePage from './pages/HomePage';
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import HomePage from "./pages/HomePage";
+import PosterPage from "./pages/PosterPage";
 
-import { Route, Switch } from 'react-router-dom';
-import Error from './pages/Error';
-import { Provider } from 'react-redux';
-import { ErrorBoundary } from 'react-error-boundary';
-import store from './redux/store';
+import { Route, Switch } from "react-router-dom";
+import Error from "./pages/Error";
+import { Provider } from "react-redux";
+import { ErrorBoundary } from "react-error-boundary";
+import store from "./redux/store";
 
 function loadUser() {
   try {
-    const token = sessionStorage.getItem('token');
+    const token = sessionStorage.getItem("token");
     if (!token) return;
 
-    store.dispatch({ type: 'auth/LOGIN_SUCCESS', payload: token });
+    store.dispatch({ type: "auth/LOGIN_SUCCESS", payload: token });
   } catch (e) {
-    console.log('SessionStorage is not working');
+    console.log("SessionStorage is not working");
   }
 }
 loadUser();
@@ -28,7 +29,8 @@ function App() {
         <Switch>
           <Route component={LoginPage} path="/login" />
           <Route component={RegisterPage} path="/register" />
-          <Route component={HomePage} path={['/']} exact />
+          <Route component={PosterPage} path="/poster" />
+          <Route component={HomePage} path={["/"]} exact />
           <Route component={NotFoundPage} />
         </Switch>
       </Provider>
---
+++ src/components/post/Poster.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Poster = () => {
+  return (
+    <>
+      <h1>poster</h1>
+      <input name="title" type="text" placeholder="title" onChange={change} />
+    </>
+  );
+};
+
+export default Poster;
